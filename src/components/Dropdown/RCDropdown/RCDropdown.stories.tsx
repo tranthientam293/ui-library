f8eeee273c35f1d68e685e4812d9958d9cd4d04b
tamtran_agl <tran_thien_tam@allgrow-labo.jp>
2023-10-12 17:45:08 +0700

    feat: update dropdown  button

--- src/components/Dropdown/RCDropdown/RCDropdown.stories.tsx
+++ src/components/Dropdown/RCDropdown/RCDropdown.stories.tsx
@@ -1,22 +1,27 @@
-import { Meta, StoryObj } from "@storybook/react"
-import { RCDropdown } from "./RCDropdown"
+import { Meta, StoryObj } from "@storybook/react";
+import { RCDropdown } from "./RCDropdown";
+import { useState } from "react";
 
 const meta = {
   title: "components/Dropdown/RCDropdown",
   tags: ["autodocs"],
   component: RCDropdown,
-} satisfies Meta<typeof RCDropdown>
+} satisfies Meta<typeof RCDropdown>;
 
-export default meta
+export default meta;
 
-type Story = StoryObj<typeof RCDropdown>
+type Story = StoryObj<typeof RCDropdown>;
 
 export const Primary: Story = {
   args: {
     menu: [
-      { key: 1, title: "Menu 1", value: 1 },
-      { key: 2, title: "Menu 2", value: 2 },
-      { key: 3, title: "Menu 3", value: 3 },
+      { key: 1, title: "Menu Item Menu Item Menu Item Menu Item 1", value: 1 },
+      { key: 2, title: "Menu Item 2", value: 2 },
+      { key: 3, title: "Menu Item 3", value: 3 },
+      { key: 4, title: "Menu Item 4", value: 4 },
+      { key: 5, title: "Menu Item 5", value: 5 },
+      { key: 6, title: "Menu Item 6", value: 6 },
     ],
+    onChange: (data) => console.log(`You clicked on "${data.title}"`),
   },
-}
+};
--- src/components/Dropdown/RCDropdown/RCDropdown.tsx
+++ src/components/Dropdown/RCDropdown/RCDropdown.tsx
@@ -1,42 +1,50 @@
-import BaseDropdown from "rc-dropdown"
-import "rc-dropdown/assets/index.css"
-import styles from "./RCDropdown.module.scss"
-import { useMemo } from "react"
+import BaseDropdown from "rc-dropdown";
+import "rc-dropdown/assets/index.css";
+import styles from "./RCDropdown.module.scss";
+import { useMemo, useRef, useState } from "react";
 
 interface MenuItem {
-  key: number
-  title: string
-  value: string | number
-  render?: (title: string, key: number) => React.ReactNode
+  key: number;
+  title: string;
+  value: string | number;
+  render?: (title: string, key: number) => React.ReactNode;
 }
 interface IRCDropdown {
-  menu?: MenuItem[]
-  overlayClassName?: string
+  menu?: MenuItem[];
+  overlayClassName?: string;
+  onChange?: (data: Omit<MenuItem, "render">) => void;
 }
 
-export function RCDropdown({ menu, overlayClassName }: IRCDropdown) {
+export function RCDropdown({ menu, overlayClassName, onChange }: IRCDropdown) {
   const overlay: React.ReactNode | undefined = useMemo(() => {
     if (menu) {
       return (
         <div className={[styles.overlay, overlayClassName].join(" ")}>
           <ul>
             {menu?.map((item) => (
-              <li key={item.key}>{item.title}</li>
+              <li
+                key={item.key}
+                onClick={() => {
+                  if (onChange !== undefined) {
+                    const { render, ...data } = item;
+                    onChange(data);
+                  }
+                }}
+              >
+                {item.title}
+              </li>
             ))}
           </ul>
         </div>
-      )
+      );
     }
-  }, [menu])
+  }, [menu]);
 
   return (
     <div className={styles.base_dropdown}>
       <BaseDropdown overlay={overlay} trigger={["click"]} animation="slide-up">
-        <div className={styles.input}>
-          <input placeholder="Chose an option" />
-          <div className={styles.chevron}></div>
-        </div>
+        <button className={styles.button}>Click me</button>
       </BaseDropdown>
     </div>
-  )
+  );
 }
