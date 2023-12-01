cf313edc5a025acf4e3c23b9c6a9133ee34c1b5c
tamtran_agl <tran_thien_tam@allgrow-labo.jp>
2023-12-01 17:47:42 +0700

    feat: install react-tooltip

---
+++ src/components/Tooltip/ReactTooltip.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Tooltip } from "react-tooltip";
+
+interface ReactTooltipProps {}
+export function ReactTooltip({}: ReactTooltipProps) {
+  return <Tooltip></Tooltip>;
+}
