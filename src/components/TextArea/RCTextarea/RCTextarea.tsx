0251062c0e6497e1df25ff05e83c6613b27d5da2
tamtran_agl <tran_thien_tam@allgrow-labo.jp>
2023-12-04 17:18:37 +0700

    feature: implemet rc-textarea

---
+++ src/components/TextArea/RCTextarea/RCTextarea.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import TextArea from "rc-textarea";
+import styles from "./RCTextarea.module.scss";
+
+interface IRCTextarea {
+  placeholder?: string;
+  minRows?: number;
+  maxRows?: number;
+  onChange?: (value?: string) => void;
+  onBlur?: (value?: string) => void;
+}
+
+export function RCTextarea({
+  placeholder,
+  minRows = 1,
+  maxRows = 1,
+  onChange,
+  onBlur,
+}: IRCTextarea) {
+  const [value, setValue] = React.useState<string | undefined>();
+
+  return (
+    <div className={styles.custom_textarea_wrapper}>
+      <TextArea
+        className={styles.custom_textarea}
+        autoSize={{ minRows: minRows, maxRows: maxRows }}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange?.(e.target.value);
+        }}
+        onBlur={(e) => onBlur?.(e.target.value)}
+      />
+      {value && <ClearButton onClick={() => setValue(undefined)} />}
+    </div>
+  );
+}
+
+function ClearButton({ onClick }: { onClick: () => void }) {
+  return (
+    <button className={styles.clear_button} onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={16}
+        height={16}
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+      </svg>
+    </button>
+  );
+}
